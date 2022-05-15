d13c1eb7f75bc51f16c69d47015cb25f3b445bd1
gstvmorais <guga.rodrigues11.0@hotmail.com>
2022-05-15 19:46:44 -0300

    added enemy, collision, projectile + smooth floor

---
+++ T1/enemies.js
@@ -0,0 +1,15 @@
+import * as THREE from "three";
+
+// create a cube
+var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+var cubeMaterial = new THREE.MeshNormalMaterial();
+export const enemy = new THREE.Mesh(cubeGeometry, cubeMaterial);
+// position the cube
+enemy.position.set(10.0, 2.0, 30);
+// add the cube to the scene
+
+export const lerpConfig = {
+  destination: new THREE.Vector3(-10.0, 0.2, -20),
+  alpha: 0.01,
+  move: true,
+};
--- T1/player.js
+++ T1/player.js
@@ -1,19 +1,21 @@
-import KeyboardState from '../libs/util/KeyboardState.js'
-import * as THREE from  'three';
-
+import KeyboardState from "../libs/util/KeyboardState.js";
+import * as THREE from "three";
+import { degreesToRadians } from "../libs/util/util.js";
+import { shoot } from "./scene.js";
 // create a cone
-const geometry = new THREE.ConeGeometry( 4, 10, 16 );
-const material = new THREE.MeshBasicMaterial( {color: 0xfeaa00} );
-export const cone = new THREE.Mesh( geometry, material );
-cone.rotation.set(0,0,0);
+const geometry = new THREE.ConeGeometry(4, 10, 16);
+const material = new THREE.MeshBasicMaterial({ color: 0xfeaa00 });
+export const cone = new THREE.Mesh(geometry, material);
+cone.rotation.set(0, 0, 0);
 
 // position the cone
-cone.position.set(0.0, 2.0, 0.0);
+cone.position.set(0.0, 4.0, 0.0);
+cone.rotateX(degreesToRadians(90));
 // add the cube to the scene
 
 // Use TextureLoader to load texture files
 var textureLoader = new THREE.TextureLoader();
-var stone = textureLoader.load('../assets/textures/floor-wood.jpg');
+var stone = textureLoader.load("../assets/textures/floor-wood.jpg");
 
 // Add texture to the 'map' property of the object's material
 cone.material.map = stone;
@@ -24,16 +26,16 @@ var keyboard = new KeyboardState();
 var clock = new THREE.Clock();
 
 export function keyboardUpdate() {
+  keyboard.update();
 
-    keyboard.update();
-  
-    var speed = 30;
-    var moveDistance = speed * clock.getDelta();
-  
-    // Keyboard.pressed - execute while is pressed
-    if ( keyboard.pressed("A"))  cone.translateX( moveDistance );
-    if ( keyboard.pressed("D"))  cone.translateX(  -moveDistance );
-    if ( keyboard.pressed("W"))  cone.translateZ(  moveDistance );
-    if ( keyboard.pressed("S"))  cone.translateZ( -moveDistance );
-    if ( keyboard.pressed("space") ) cone.position.set(0.0, 2.0, 0.0);
-  }
+  var speed = 30;
+  var moveDistance = speed * clock.getDelta();
+
+  // Keyboard.pressed - execute while is pressed
+  if (keyboard.pressed("A")) cone.translateX(moveDistance);
+  if (keyboard.pressed("D")) cone.translateX(-moveDistance);
+  if (keyboard.pressed("W")) cone.translateY(moveDistance);
+  if (keyboard.pressed("S")) cone.translateY(-moveDistance);
+  if (keyboard.pressed("F")) cone.position.set(0.0, 2.0, 0.0);
+  if (keyboard.pressed("space")) shoot();
+}
