a99c381b83f0580bfcbf1a707da5807ac820d565
Piazzi <40416044+Piazzi@users.noreply.github.com>
2022-06-21 15:36:58 -0300

    add comments

--- T2/js/interface.js
+++ T2/js/interface.js
@@ -3,6 +3,10 @@ import Stats from "../../build/jsm/libs/stats.module.js";
 
 const lifeInterface = document.getElementById("life");
 
+/**
+ * Remove the num (parameter) of hearts from the interface
+ * @param {int} num 
+ */
 export function removeHearts(num) {
   for (let i = 0; num != 0; i++) {
     if (lifeInterface.children[i].style.display != "none") {
@@ -12,21 +16,24 @@ export function removeHearts(num) {
   }
 }
 
+/**
+ * Reset hearts to its default state, 5 hearts.
+ */
 export function resetHearts() {
   for (let i = 0; i < lifeInterface.children.length; i++) {
     lifeInterface.children[i].style.display = "";
   }
 }
 
+export const stats = new Stats();
+export var clock = new Clock(true);
+
+let clockText = document.getElementById("clock");
 // show clock time
 setInterval(() => {
   clockText.innerText = clock.getElapsedTime().toFixed();
 }, "1000");
 
-export const stats = new Stats();
-export var clock = new Clock(true);
-
-let clockText = document.getElementById("clock");
 let statsText = document.getElementById("stats");
 stats.dom.style.top = "180px";
 stats.dom.style.marginLeft = "20px";
