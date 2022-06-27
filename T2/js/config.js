a706932d32a5bd7b9f3109be3fad022e0a182e48
Anthony-a-l-s <alima.als37@gmail.com>
2022-06-26 22:34:46 -0300

    add ground enemy

--- T2/js/config.js
+++ T2/js/config.js
@@ -13,30 +13,36 @@ export const SCREEN_BOTTOM_EDGE = -15;
 export var HEIGHT = 10;
 export const SPAWN_TYPE_1_ENEMY_INTERVAL = 3000; //  spawns every x milliseconds
 export const SPAWN_TYPE_2_ENEMY_INTERVAL = 5000; 
-export const SPAWN_TYPE_3_ENEMY_INTERVAL = 6000; 
+export const SPAWN_TYPE_3_ENEMY_INTERVAL = 5000; 
 export const SPAWN_TYPE_4_ENEMY_INTERVAL = 8000; 
+export const SPAWN_TYPE_5_ENEMY_INTERVAL = 6000; 
 export const SPAWN_HEALTHPACK_INTERVAL = 15000; 
 export const SHOOT_COOLDOWN = 500; // decresase every X milliseconds
 export const MIN_ENEMY_SPEED = -0.1;
 export const MAX_ENEMY_SPEED = -0.3;
-export const ENEMY_PROJECTILE_SPEED = 0.02;
+export const GROUND_ENEMY_SPEED = -0.06;
+export const ENEMY_PROJECTILE_SPEED = 0.3;
 export const PLAYER_PROJECTILE_SPEED = 0.9;
 export const PLAYER_SPEED = 30;
 export const PLANE_SPEED = 0.1;
+export var GROUND_ENEMY_POSITION = 1.5;
 export let ENEMY_1_INTERVAL_HANDLER;
 export let ENEMY_2_INTERVAL_HANDLER;
 export let ENEMY_3_INTERVAL_HANDLER;
 export let ENEMY_4_INTERVAL_HANDLER;
+export let ENEMY_5_INTERVAL_HANDLER;
 export let HEALTHPACK_INTERVAL_HANDLER;
 export let HEALTHPACK_TIMEOUT_HANDLER;
 export let ENEMY_1_TIMEOUT_HANDLER;
 export let ENEMY_2_TIMEOUT_HANDLER;
 export let ENEMY_3_TIMEOUT_HANDLER;
 export let ENEMY_4_TIMEOUT_HANDLER;
+export let ENEMY_5_TIMEOUT_HANDLER;
 const SPAWN_TYPE_1_ENEMY_AFTER = 1000; //  spawns after X milliseconds passed on clock
 const SPAWN_TYPE_2_ENEMY_AFTER = 10000; 
 const SPAWN_TYPE_3_ENEMY_AFTER = 30000; 
 const SPAWN_TYPE_4_ENEMY_AFTER = 60000; 
+const SPAWN_TYPE_5_ENEMY_AFTER = 1000; 
 const SPAWN_HEALTHPACK_AFTER = 15000;
 
 export function resetTimeouts() {
@@ -70,6 +76,14 @@ export function resetTimeouts() {
       spawnEnemy(4);
     }, SPAWN_TYPE_4_ENEMY_INTERVAL);
   }, SPAWN_TYPE_4_ENEMY_AFTER);
+
+  ENEMY_5_TIMEOUT_HANDLER = setTimeout(() => {
+    ENEMY_5_INTERVAL_HANDLER = setInterval(() => {
+      spawnEnemy(5);
+    }, SPAWN_TYPE_5_ENEMY_INTERVAL);
+  }, SPAWN_TYPE_5_ENEMY_AFTER);
 }
+
+
 
 resetTimeouts();
--- T2/js/enemy.js
+++ T2/js/enemy.js
@@ -17,10 +17,13 @@ import {
   MIN_ENEMY_SPEED,
   OFF_SCREEN_BOTTOM,
   OFF_SCREEN_TOP,
+  OFF_SCREEN_LEFT,
   OFF_SCREEN_RIGHT,
   SCREEN_LEFT_EDGE,
   SCREEN_RIGHT_EDGE,
   ENEMY_PROJECTILE_SPEED,
+  GROUND_ENEMY_POSITION,
+  GROUND_ENEMY_SPEED
 } from "./config.js";
 import { degreesToRadians } from "../../libs/util/util.js";
 
@@ -82,39 +85,48 @@ export function spawnEnemy(type) {
   let enemy2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
   let enemy3 = new THREE.Mesh(cubeGeometry, cubeMaterial);
   let enemy4 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  let enemy5 = new THREE.Mesh(cubeGeometry, cubeMaterial);
   let randomPosition = getRandomNumber(SCREEN_LEFT_EDGE, SCREEN_RIGHT_EDGE);
   let randomSpeed = getRandomNumber(MIN_ENEMY_SPEED, MAX_ENEMY_SPEED);
   if (type === 1) {
     spawn(enemy, randomPosition, HEIGHT, OFF_SCREEN_TOP);
     setTimeout(() => {
-      shootEnemy(enemy);
+      shootEnemy(enemy, 1);
     }, "500");
   }
   if (type === 2) {
     spawn(enemy2, OFF_SCREEN_RIGHT, HEIGHT, randomPosition);
     setTimeout(() => {
-      shootEnemy(enemy2);
+      shootEnemy(enemy2, 1);
     }, "500");
   }
   if (type === 3) {
     spawn(enemy3, OFF_SCREEN_RIGHT, HEIGHT, OFF_SCREEN_TOP);
     setTimeout(() => {
-      shootEnemy(enemy3);
+      shootEnemy(enemy3, 1);
     }, "500");
   }
 
   if (type === 4) {
     spawn(enemy4, OFF_SCREEN_RIGHT, HEIGHT, 20);
     setTimeout(() => {
-      shootEnemy(enemy4);
+      shootEnemy(enemy4, 1);
     }, "500");
   }
+
+  if (type === 5) {
+    spawn(enemy5, randomPosition, GROUND_ENEMY_POSITION, OFF_SCREEN_TOP);
+    setTimeout(() => {
+      shootEnemy(enemy, 2);
+    }, "2000");
+  }
+  
   // every 10 ms checks if the enemy hit the player or exit the screen
 
   setInterval(() => {
     if (playerLife <= 0) {
       resetGame();
-      increasePlayerLife(5);
+      increasePlayerLife(5, 2);
       return;
     }
     if (type === 1) {
@@ -132,6 +144,9 @@ export function spawnEnemy(type) {
       enemy4.translateX(randomSpeed);
       enemy4.rotateY(degreesToRadians(0.25));
     }
+    if (type === 5) {
+      enemy5.translateZ(GROUND_ENEMY_SPEED);
+    }
     // remove the enemy if exits the screen
     if (enemy.position.z <= OFF_SCREEN_BOTTOM) {
       scene.remove(enemy);
@@ -144,9 +159,13 @@ export function spawnEnemy(type) {
 
   return;
 }
-export function shootEnemy(object) {
+export function shootEnemy(object, type) {
   // creates the projectile
-  var sphereGeometry = new THREE.SphereGeometry(0.6, 16, 8);
+  var sphereGeometry;
+  if(type === 2)
+  sphereGeometry = new THREE.CylinderGeometry(0.3, 0.3, 2.0, 25);
+  else  
+  sphereGeometry = new THREE.SphereGeometry(0.6, 16, 8);
   var sphereMaterial = new THREE.MeshLambertMaterial({ color: "#FEFE00" });
   var enemyProjectile = new THREE.Mesh(sphereGeometry, sphereMaterial);
   enemyProjectile.alive = true;
@@ -162,20 +181,32 @@ export function shootEnemy(object) {
     player.position.y,
     player.position.z 
   );
+  enemyProjectile.lookAt(projectileDestination);
   scene.add(enemyProjectile);
+  if (type === 2) {
+    enemyProjectile.rotateX(degreesToRadians(90))
+     setInterval(() => {
+       if (enemyProjectile.position.y <= HEIGHT) {
+        enemyProjectile.translateY(0.1);
+       }
+     }, "5");
+   }
   // every 10 ms checks if the projectileEnemy hit any object or exit the screen
   setInterval(() => {
-    enemyProjectile.position.lerp(projectileDestination, ENEMY_PROJECTILE_SPEED);
+    if (enemyProjectile.position.y >= HEIGHT ) {
+      enemyProjectile.translateZ(ENEMY_PROJECTILE_SPEED);
+    }
+   
     // remove the projectile if exits the screen
     if (
-      Math.trunc(enemyProjectile.position.z) >
-      Math.trunc(projectileDestination.z - 2) &&
-      Math.trunc(enemyProjectile.position.z) <
-      Math.trunc(projectileDestination.z + 2) 
+     enemyProjectile.position.z <= OFF_SCREEN_BOTTOM ||
+     enemyProjectile.position.z >= OFF_SCREEN_TOP ||
+     enemyProjectile.position.x <= OFF_SCREEN_LEFT ||
+     enemyProjectile.position.x >= OFF_SCREEN_RIGHT 
     ) {
       scene.remove(enemyProjectile);
       projectileEnemies = projectileEnemies.filter(
-        (p) => p.id !== enemyProjectile.id
+        (p) => p.id !==enemyProjectile.id
       );
       return;
     }
@@ -190,19 +221,12 @@ export function shootEnemy(object) {
       increasePlayerLife(-1);
       removeHearts(1);
       }
-      shrink(object);
       enemyProjectile.alive = false;
       scene.remove(enemyProjectile);
       enemyProjectile.position.set(0, 0, OFF_SCREEN_BOTTOM);
       shrink(playerGeometry);
       grow(playerGeometry);
       // wait 200ms before removing the object so that the animation can play
-      setTimeout(function () {
-        object.position.set(0, 0, OFF_SCREEN_BOTTOM);
-        scene.remove(object);
-
-        return;
-      }, 200);
     }
   }, "10");
   return;
